f33b78d7f8f828c114025ac104b05df3ae7b555d
Bohdan Borodavka <bbdann98@gmail.com>
2020-02-02 20:54:23 +0200

    Updates

---
+++ precache-manifest.42c45f9eee055d28e2aa1addff6050c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ecaa3acd9638a55789a2a6ae528d356",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "e640fd198567536a4cad",
+    "url": "/my-app/static/css/main.2cccf87d.chunk.css"
+  },
+  {
+    "revision": "77a9be50acf5469fe081",
+    "url": "/my-app/static/js/2.0468e89e.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/my-app/static/js/2.0468e89e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e640fd198567536a4cad",
+    "url": "/my-app/static/js/main.1e3ae577.chunk.js"
+  },
+  {
+    "revision": "279727620455059822d9",
+    "url": "/my-app/static/js/runtime-main.dcb7773c.js"
+  }
+]);
